e47fcc23095b69628bd4d90aea0c4504644d70e4
Steven van der Merwe <stevenvdmerwe@gmail.com>
2014-03-14 00:08:27 +0200

    Changed target location of wkhtmltopdf executable
    local binaries target modificaiton

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var server = http.createServer(function(request, response) {
     var parsedUrl = url.parse(request.url, true); // true to get query as object
     var queryAsObject = parsedUrl.query;
     var filePath = 'output.pdf';
-    wkhtmltopdf.command = 'c:/wkhtmltopdf/bin/wkhtmltopdf.exe';
+    wkhtmltopdf.command = './wkhtmltopdf/bin/wkhtmltopdf.exe';
     wkhtmltopdf(queryAsObject["link"], {output: filePath}, function(){
         var stat = fileSystem.statSync(filePath);
 
